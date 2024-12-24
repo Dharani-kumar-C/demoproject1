32db6dd3e5d71488154ad5e7b65f4e8a564bb67f
Dharani-kumar-C <kumardharani863@gmail.com>
2024-12-24 18:07:01 +0530

    added

---
+++ resources/views/welcomepage.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite("resources/css/app.css")
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <div class="flex p-6 bg-blue-500 text-white">
+        <div class="pr-20">
+            <h1>Logo</h1>
+        </div>
+        <div class="flex  space-x-10">
+            <a class="text-light text-decoration-none underline" href="">Home</a>
+            <a class="text-light text-decoration-none underline" href="">About</a>
+            <a class="text-light text-decoration-none underline" href="">Contact</a>
+        </div>
+    </div>
+
+</header>
+<div class="m-20 bg-blue-500 text-white p-24 rounded-2xl">
+    <div class="flex">
+        <h1>Hi , Welcome to the webpage</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class=" size-8">
+            <path d="M9.375 3a1.875 1.875 0 0 0 0 3.75h1.875v4.5H3.375A1.875 1.875 0 0 1 1.5 9.375v-.75c0-1.036.84-1.875 1.875-1.875h3.193A3.375 3.375 0 0 1 12 2.753a3.375 3.375 0 0 1 5.432 3.997h3.943c1.035 0 1.875.84 1.875 1.875v.75c0 1.036-.84 1.875-1.875 1.875H12.75v-4.5h1.875a1.875 1.875 0 1 0-1.875-1.875V6.75h-1.5V4.875C11.25 3.839 10.41 3 9.375 3ZM11.25 12.75H3v6.75a2.25 2.25 0 0 0 2.25 2.25h6v-9ZM12.75 12.75v9h6.75a2.25 2.25 0 0 0 2.25-2.25v-6.75h-9Z" />
+        </svg>
+    </div>
+
+    <p></p>
+</div>
+</body>
+</html>
